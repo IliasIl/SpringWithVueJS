8708d0492475a1a323ccf73da8d65f648ae57f1f
Ilias <cossin90@gmail.com>
2019-07-01 20:01:00 +0300

    Created new project.

---
+++ src/main/java/space/ilias/SpringWithVueJS/SpringWithVueJsApplication.java
@@ -0,0 +1,13 @@
+package space.ilias.SpringWithVueJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithVueJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithVueJsApplication.class, args);
+	}
+
+}
