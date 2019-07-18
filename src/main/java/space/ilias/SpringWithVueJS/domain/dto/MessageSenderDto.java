a18715df8afba421a8ef995d0c53cf679209daed
Ilias <cossin90@gmail.com>
2019-07-18 21:12:42 +0300

    Eliminated non-used code.

--- src/main/java/space/ilias/SpringWithVueJS/domain/dto/MessageSenderDto.java
+++ src/main/java/space/ilias/SpringWithVueJS/domain/dto/MessageSenderDto.java
@@ -1,14 +1,12 @@
 package space.ilias.SpringWithVueJS.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.codehaus.jackson.annotate.JsonRawValue;
-import space.ilias.SpringWithVueJS.domain.Views;
 
 @Data
 @AllArgsConstructor
-@JsonView(Views.IdName.class)
+//@JsonView(Views.IdName.class)
 public class MessageSenderDto {
     private ObjectType objectType;
     private EventClass eventClass;
